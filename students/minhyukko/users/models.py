fee5ba6ce57f1f655db8410262f6dda3a9554231
minhyukko <komhgogo@gmail.com>
2021-11-18 18:52:10 +0900

    users.models created, udated 필드값 추가

--- students/minhyukko/users/models.py
+++ students/minhyukko/users/models.py
@@ -8,5 +8,8 @@ class User(models.Model):
     password    = models.CharField(max_length=300)
     address     = models.CharField(max_length=20)
     information = models.CharField(max_length= 250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
     class Meta:
         db_table = 'users'
