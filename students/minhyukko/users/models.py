2f6445d9adf338bd55e9d94b20c87bab14dd14bf
minhyukko <komhgogo@gmail.com>
2021-11-18 19:44:41 +0900

    users.models name, address 필드값 수정

--- students/minhyukko/users/models.py
+++ students/minhyukko/users/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    name        = models.CharField(max_length= 20)
+    name        = models.CharField(max_length= 100)
     email       = models.CharField(max_length=250 , unique= True)
     password    = models.CharField(max_length=300)
-    address     = models.CharField(max_length=20)
+    address     = models.CharField(max_length=100)
     information = models.CharField(max_length= 250)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
