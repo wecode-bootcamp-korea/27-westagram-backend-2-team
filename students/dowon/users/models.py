5ee73404cdc1cd9bba1f78132462e3905269c61d
장도원 <jangdowon@dopago.local>
2021-11-18 17:52:39 +0900

    [장도원] Mission 2: 모델링

--- students/dowon/users/models.py
+++ students/dowon/users/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from django.db import models
 
 class User(models.Model):
@@ -9,8 +10,8 @@ class User(models.Model):
 
     class Meta:
         db_table = 'users'
-
+    
     def __str__(self):
 	    return self.name
-        
+
 # Create your models here.
