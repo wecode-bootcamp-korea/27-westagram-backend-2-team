1093c02df90825e40508ebf9513e72d4250fb5dc
장도원 <jangdowon@dopago.local>
2021-11-18 17:15:28 +0900

    [장도원] Mission 2: 모델링

---
+++ students/dowon/westagram/users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45, unique=True)
+    password = models.CharField(max_length=45)
+    phone = models.IntegerField(unique=True)
+    information = models.CharField(max_length=300)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+	    return self.name
+        
+# Create your models here.
