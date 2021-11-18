a9b8695604540261acb40b0a5ebca188f8fa7ac8
장도원 <jangdowon@dopago.local>
2021-11-18 20:08:05 +0900

    [장도원] Mission 2: 모델링

--- students/dowon/users/models.py
+++ students/dowon/users/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class User(models.Model):
-    name		= models.CharField(max_length=45)
-    email		= models.CharField(max_length=45, unique=True)
-    password	= models.CharField(max_length=200)
-    phone_number= models.CharField(unique=True)
-    information	= models.CharField(max_length=300)
+	name		= models.CharField(max_length=45)
+	email		= models.CharField(max_length=45, unique=True)
+	password	= models.CharField(max_length=200)
+	phone_number= models.CharField(max_length=45, unique=True)
+	information	= models.CharField(max_length=300)
 	created_at	= models.DateTimeField(auto_now_add=True)
 	updated_at	= models.DateTimeField(auto_now=True)
-    
+	
 	class Meta:
-        db_table = 'users'
+		db_table = 'users'
     
-# Create your models here.
+# Create: your models here.
