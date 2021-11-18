359b031f271665cc4f03df6cd525bf92db7d52a2
장도원 <jangdowon@dopago.local>
2021-11-18 18:36:27 +0900

    [장도원] Mission 2: 모델링

--- students/dowon/users/models.py
+++ students/dowon/users/models.py
@@ -1,10 +1,9 @@
-from enum import unique
 from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=45)
     email = models.CharField(max_length=45, unique=True)
-    password = models.CharField(max_length=45)
+    password = models.CharField(max_length=200)
     phone = models.IntegerField(unique=True)
     information = models.CharField(max_length=300)
 
