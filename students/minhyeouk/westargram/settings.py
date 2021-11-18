737ddcc2ebbacfff53a53edb7658a157f8dbd901
유민혁 <93bulldozer@yuminhyeog-ui-MacBookAir.local>
2021-11-18 16:11:31 +0900

    project initial setting 수정본1

--- students/minhyeouk/westargram/settings.py
+++ students/minhyeouk/westargram/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ca(e&1o6*(ql9kc141hk@=u9)5ax5qnlak3*c@h6(oq=^ip82d'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,12 +80,7 @@ WSGI_APPLICATION = 'westargram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DATABASES
 
 
 # Password validation
