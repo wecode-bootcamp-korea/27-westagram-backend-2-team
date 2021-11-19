518bd7e10373a4a2682893e41f52148494ea54b5
jumyeong <jm01028250379@gmail.com>
2021-11-19 05:51:11 -0500

    order models.py

--- students/jumyoeng/users/models.py
+++ students/jumyoeng/users/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class User(models.Model) :
-    name                = models.CharField(max_length=45)
-    email                 = models.EmailField(max_length=45)
-    password          = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=20)
-    description        = models.CharField(max_length=255)
-    created_at         = models.DateTimeField(auto_now_add=True)
-    updated_at        = models.DateTimeField(auto_now=True)
+    name         = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=45)
+    password     = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20, null=True)
+    description  = models.CharField(max_length=255)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
 
     class Meta : 
         unique_together = ('name', 'email')
-        db_table              = 'users'
+        db_table        = 'users'
