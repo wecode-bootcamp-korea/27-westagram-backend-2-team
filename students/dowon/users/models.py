2f9cccc2b6cad8e9eeb9faa069b9d0eedd8d6f59
장도원 <jangdowon@dopago.local>
2021-11-19 17:57:51 +0900

    [장도원] Mission 3: 회원가입 기능 구현

--- students/dowon/users/models.py
+++ students/dowon/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
 	name         = models.CharField(max_length=45)
 	email        = models.CharField(max_length=45, unique=True)
 	password     = models.CharField(max_length=200)
-	phone_number = models.CharField(max_length=45, unique=True)
+	phone_number = models.CharField(max_length=45)
 	information  = models.CharField(max_length=300)
 	created_at   = models.DateTimeField(auto_now_add=True)
 	updated_at   = models.DateTimeField(auto_now=True)
