6c08ea34a9c109940a71ab96e0498655cbf10a14
jumyeong <jm01028250379@gmail.com>
2021-11-21 03:34:08 -0500

    rebase mode1

---
+++ students/jumyoeng/users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from users.views import UserView
+
+urlpatterns = [
+    path('create', UserView.as_view())
+]
+
--- students/jumyoeng/users/views.py
+++ students/jumyoeng/users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+from users.models import User
+from users.lib.validation import email_validation, password_validation
+
+class UserView(View) :
+    def post(self, request):
+        try :
+            data = json.loads(request.body)
+            if User.objects.filter(email=data['email']) :
+                return JsonResponse({'MESSAGE': 'EMAIL_DUPLICATE'}, status=400)
+
+            if not email_validation(data['email']) :
+                return JsonResponse({'MESSAGE': 'EMAIL_INVALID'}, status=400)
+
+            if not password_validation(data['password']) :
+                return JsonResponse({'MESSAGE': 'PASSWORD_INVALID'}, status=400)
+
+            User.objects.create(
+            name                 = data['name'],
+            email                 = data['email'],
+            password          = data['password'],
+            phone_number = data['phone'],
+            description        = data['descr'],
+            )
+            return JsonResponse({'MESSAGE': 'CREATED'}, status=201)
+
+        except KeyError:
+            return  JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
