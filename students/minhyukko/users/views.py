9a8ab1c7c76645002fd0c477804abce9add14b52
minhyukko <komhgogo@gmail.com>
2021-11-22 15:55:21 +0900

    users.views에 error 처리방법 오타 수정

--- students/minhyukko/users/views.py
+++ students/minhyukko/users/views.py
@@ -33,7 +33,7 @@ class UserView(View):
             return JsonResponse({'message':'SUCESS'},status=201)
 
         except ValidationError as e:
-            return JsonResponse({'message': e.mmessage},status=400)
+            return JsonResponse({'message': e.message},status=400)
 
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
