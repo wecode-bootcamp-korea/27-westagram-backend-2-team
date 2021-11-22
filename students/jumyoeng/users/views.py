87fc400bc2c3b51d8bc8d104210aff8f762334cb
jumyeong <jm01028250379@gmail.com>
2021-11-21 23:32:12 -0500

    Fixed up
    -  change urls.py
    - change view.py class name
    - fix phone_number could get null, as default
    - add except Exception fo other errors

--- students/jumyoeng/users/views.py
+++ students/jumyoeng/users/views.py
@@ -2,13 +2,15 @@ import json
 
 from django.http import JsonResponse
 from django.views import View
+
 from users.models import User
 from users.lib.validation import email_validation, password_validation
 
-class UserView(View) :
+class SignUpView(View) :
     def post(self, request):
         try :
             data = json.loads(request.body)
+
             if User.objects.filter(email=data['email']) :
                 return JsonResponse({'MESSAGE': 'EMAIL_DUPLICATE'}, status=400)
 
@@ -22,10 +24,13 @@ class UserView(View) :
             name         = data['name'],
             email        = data['email'],
             password     = data['password'],
-            phone_number = data['phone'],
+            phone_number = None if data.get('phone') is None else data['phone'],
             description  = data['descr'],
             )
             return JsonResponse({'MESSAGE': 'CREATED'}, status=201)
 
         except KeyError:
             return  JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
+        except Exception as e :
+            print(e)
