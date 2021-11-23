4c264ea6a0bbf64e20d80abf29c9170f44586846
jumyeong <jm01028250379@gmail.com>
2021-11-23 01:59:14 -0500

    modified by feedback
    - simple structure for validation.py

--- students/jumyoeng/users/my_lib/validation.py
+++ students/jumyoeng/users/my_lib/validation.py
@@ -1,19 +1,13 @@
 import re 
 
+from django.core.exceptions import ValidationError
+
 def email_validation(email) :
     em_form =r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-    vaild = re.search(em_form , email)
-    return vaild
+    if not re.search(em_form , email) :
+        raise ValidationError('EMAIL_INVALID')
 
 def password_validation(password) :
-    if len(password) < 8 :
-        return False
-    elif re.search('[\d]+', password) is None:
-        return False
-    elif re.search('[\D]+', password) is None:
-        return False
-    elif re.search('[,./~!@#$%^&*()><]+', password) is None:
-        return False
-    else :
-        return True
- 
+    pw_form = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+    if not re.match(pw_form, password) :
+        raise ValidationError('PASSWORD_INVALID')
