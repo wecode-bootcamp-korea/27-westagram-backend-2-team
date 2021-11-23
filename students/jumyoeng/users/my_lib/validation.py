ba992dca214d413eadb72438a77dbb28474af2f2
jumyeong <jm01028250379@gmail.com>
2021-11-23 02:28:27 -0500

    Modified by feedback[2]
    - abbreviation word to full word
    - re.search() to re.match()

--- students/jumyoeng/users/my_lib/validation.py
+++ students/jumyoeng/users/my_lib/validation.py
@@ -3,11 +3,11 @@ import re
 from django.core.exceptions import ValidationError
 
 def email_validation(email) :
-    em_form =r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-    if not re.search(em_form , email) :
+    email_form =r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if not re.match(email_form , email) :
         raise ValidationError('EMAIL_INVALID')
 
 def password_validation(password) :
-    pw_form = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
-    if not re.match(pw_form, password) :
+    password_form = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+    if not re.match(password_form, password) :
         raise ValidationError('PASSWORD_INVALID')
