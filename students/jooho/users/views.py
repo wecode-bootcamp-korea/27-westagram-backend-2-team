1c85c2d77b306faa51661080e0d0c7abf81e52e6
joshua <krjoshua21@gmail.com>
2021-11-23 17:36:42 +0900

    signup2nd

--- students/jooho/users/views.py
+++ students/jooho/users/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+import re, json
 
-# Create your views here.
+from django.http.response   import JsonResponse
+from django.views           import View
+from django.core.exceptions import ValidationError
+
+from .models                import User
+
+class SignUpView(View):
+    def post(self, request):
+        data         = json.loads(request.body)
+        
+        try:
+            name         = data["name"]
+            email        = data['email']
+            password     = data["password"]
+            phone_number = data["phone_number"]
+            birthday     = data.get('birthday', None)
+            gender       = data.get('gender', None)           
+            
+            regex_email    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9-.]+$'
+            regex_password = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}'
+            regex_phone    = '/^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$/'
+                            
+            
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({"message" : "EMAIL_ALREADY_EXISTS"}, status=400)
+
+            if not re.match(regex_email, email):
+                return JsonResponse({"message": "EMAIL_ERROR"}, status=400)
+
+            if not re.match(regex_password, password):
+                return JsonResponse({"message": "PASSWORD_ERROR"}, status=400)
+            
+            if not re.match(regex_phone, phone_number):
+                return JsonResponse({'message': 'INVALID_CONTACT'}, status=400)
+
+            User.objects.create(
+                name         = name,
+                email        = email,
+                password     = password,
+                phone_number = phone_number,
+                birthday     = birthday,
+                gender       = gender,
+                )
+            return JsonResponse({"message": "Success"}, status = 201)
+        
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+   
