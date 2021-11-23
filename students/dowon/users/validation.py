933f947bab181e6e730be8051c72a409e28f9a47
장도원 <jangdowon@dopago.local>
2021-11-23 15:55:57 +0900

    Chore: validation 함수 if not 코드 수정

--- students/dowon/users/validation.py
+++ students/dowon/users/validation.py
@@ -8,11 +8,11 @@ regex_email    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9-.]+$'
 regex_password = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}'
 
 def validate_email(email):
-    if re.match(regex_email, email) is None:
+    if not re.match(regex_email, email):
         raise ValidationError('INVALID_EMAIL')
     if User.objects.filter(email=email).exists():
         raise ValidationError('ALREADY_EXISTS')
 
 def validate_password(password):
-    if re.match(regex_password, password) is None:
+    if not re.match(regex_password, password):
         raise ValidationError('INVALID_PASSWORD')
