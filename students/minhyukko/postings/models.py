3b253a346b1e884dd5814d3d84f6a5d0267bf714
minhyukko <komhgogo@gmail.com>
2021-11-24 14:46:53 +0900

    임시저장

--- students/minhyukko/postings/models.py
+++ students/minhyukko/postings/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-from users.models import Users
+from users.models import User
 
 class Posting(models.Model): 
-    user       = models.ForeignKey(Users, on_delete= models.CASCADE)
+    user       = models.ForeignKey(User, on_delete= models.CASCADE)
     image      = models.CharField(max_length=300)
     post       = models.CharField(max_length=300, null = True)
     created_at = models.DateTimeField(auto_now_add=True)
