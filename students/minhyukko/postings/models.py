6a740231564dd0f99828d5bc99d2eae995ff3aa7
minhyukko <komhgogo@gmail.com>
2021-11-24 09:44:48 +0900

    임시저장

---
+++ students/minhyukko/postings/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from uesrs.models import Users
+
+class Posting(models.Model): 
+    user       = models.ForeignKey(Users, on_delete= models.CASCADE)
+    image      = models.CharField(max_length=300)
+    post       = models.CharField(max_length=300, null = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'postings'
